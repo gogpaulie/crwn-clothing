c24307d867f0f37a548bf1acc2076abef84284fe
GoG_Paulie <ppolih21@gmail.com>
2021-09-03 19:48:45 -0600

    add sign in form and custom button

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButtom from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type='email'
+            name='email'
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label='Email'
+            required
+          />
+          <FormInput
+            type='password'
+            name='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='Password'
+            required
+          />
+          <CustomButtom type='submit'>Sign in</CustomButtom>
+        </form>
+      </div>
+    );
+  }
+}
